5ab371e0757dd32efb004bb876cf3d5d440fee1f
miyagon <miyagon@example.com>
2019-08-19 16:40:55 +0900

    update: get templates from MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+const { Templates } = require('./src/server/templates');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/api', (req, res) => {
-  res.send({ api: 'test' });
-})
+app.get('/api/templates/get', async (req, res) => {
+  res.send(await Templates.get());
+});
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html')
-})
+});
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
   console.log('server running');
 });
---
+++ src/server/templates.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://mongo:27017/';
+const options = {
+  auth: {
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+const Templates = {
+  get: async () => {
+    const client = new MongoClient(url, options);
+
+    try {
+      // Connect to MongoDB
+      await client.connect();
+
+      // Database
+      const db = client.db('templates');
+
+      // Find collection `customers`
+      const result = await db.collection('customers').find().toArray();
+
+      return JSON.stringify(result);
+    } catch (err) {
+      console.log(err.stack);
+      return '{ "error": "Failed to connect MongoDB" }';
+    } finally {
+      if (client) client.close();
+    }
+  },
+  insert: async customerName => {
+
+  },
+  update: async customerName => {
+
+  }
+};
+
+module.exports.Templates = Templates;
