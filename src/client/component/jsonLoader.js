3419f8b6161352263820ad0c10a109e351298f6e
miyagon <miyagon@example.com>
2019-08-24 13:08:38 +0900

    clean: refactor readAsJson()

--- src/client/component/jsonLoader.js
+++ src/client/component/jsonLoader.js
@@ -11,7 +11,11 @@ export default class JsonLoader extends React.Component {
       id="droparea"
       onDragEnter={ cancelEvent }
       onDragOver={ cancelEvent }
-      onDrop={ readAsJson(this.props.onDrop) }
+      onDrop={ e => {
+      	e.preventDefault();
+        const file = e.dataTransfer.files[0];// TODO: 複数ファイル
+        readAsJson(file, this.props.onDrop);
+      }}
     >
       <p>Drop json here!</p>
     </div>
@@ -31,16 +35,11 @@ function cancelEvent(e) {
 
 /**
  * Read dropped file as json, and pass json to callback function
- * @param  {Function} callback call it with read json
+ * @param  {Object}   file file will be read
+ * @param  {Function} callback function called with read json
  */
-function readAsJson(callback) {
-  return e => {
-  	e.preventDefault();
-
-  	const file = e.dataTransfer.files[0];// TODO: 複数ファイル
-
-  	const reader = new FileReader();
-  	reader.onload = e => { callback(JSON.parse(e.target.result)) };
-  	reader.readAsText(file);
-  };
+function readAsJson(file, callback) {
+	const reader = new FileReader();
+	reader.onload = e => { callback(JSON.parse(e.target.result)) };
+	reader.readAsText(file);
 }
