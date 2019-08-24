f9556840887d48abb1caa0cdb95c52e5eea34e60
miyagon <miyagon@example.com>
2019-08-24 13:08:52 +0900

    clean: remove console.log()

--- src/client/component/templates-header.js
+++ src/client/component/templates-header.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class TemplatesHeader extends React.Component {
   render() {
-    console.log(this.props);
     return (
       <>
         <button onClick={ e => {
