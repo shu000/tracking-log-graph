0f394ce088d7c75d713c08b819a6307ba7e03032
miyagon <miyagon@example.com>
2019-09-04 23:16:37 +0900

    clean: indent

--- src/client/action/customers.js
+++ src/client/action/customers.js
@@ -34,19 +34,46 @@ export function fetchCustomers() {
 
   return function(dispatch) {
     return fetch(URL, {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(
+      response => response.json(),
+      error => console.log(error)
+    )
+    .then(json => {
+      if (json.error) dispatch(receiveError(json.error));
+      else dispatch(receiveCustomers(json.result));
+    })
+  }
+}
+
+const URL_ADD = 'http://localhost:8080/api/customers/add';
+export function addCustomer(customerName) {
+
+  //dispatch(Loading...)
+
+  return function(dispatch) {
+    return fetch(URL_ADD, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customerName: customerName
       })
-      .then(
-        response => response.json(),
-        error => console.log(error)
-      )
-      .then(json => {
-        if (json.error) dispatch(receiveError(json.error));
-        else dispatch(receiveCustomers(json.result));
-      })
+    })
+    .then(
+      response => response.json(),
+      error => console.log(error)
+    )
+    .then(json => {
+      if (json.error) dispatch(receiveError(json.error));
+      else dispatch(fetchCustomers());
+    })
   }
 }
