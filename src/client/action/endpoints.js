e37788ffc5af81db84777f8bdbb09564f2a8e925
miyagon <miyagon@example.com>
2019-09-09 22:36:59 +0900

    fix: endpoint

--- src/client/action/endpoints.js
+++ src/client/action/endpoints.js
@@ -1,13 +1,11 @@
-const HOST = 'http://127.0.0.1';
-const PORT = process.env.APP_PORT;
-const api_endpoint = HOST + ':' + PORT + '/api/';
+const API_ENDPOINT = 'http://127.0.0.1/api';
 
-export const GET_CUSTOMERS = api_endpoint + 'customers';
+export const GET_CUSTOMERS = API_ENDPOINT + '/customers';
 
-export const ADD_CUSTOMER =  api_endpoint + 'customers/add';
+export const ADD_CUSTOMER =  API_ENDPOINT + '/customers/add';
 
-export const DELETE_CUSTOMER = api_endpoint + 'customers/delete';
+export const DELETE_CUSTOMER = API_ENDPOINT + '/customers/delete';
 
-export const GET_TEMPLATE = api_endpoint + 'template';
+export const GET_TEMPLATE = API_ENDPOINT + '/template';
 
-export const UPDATE_TEMPLATE = api_endpoint + 'template/update';
+export const UPDATE_TEMPLATE = API_ENDPOINT + '/template/update';
